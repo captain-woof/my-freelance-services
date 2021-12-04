2523f1ea2e6eb3d4a151628f04e6c1453f020c7b
Sohail Saha <captainwoof.official@gmail.com>
2021-12-05 02:05:44 +0530

    SSR for MUI styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
                     <meta name="theme-color" content={theme.palette.primary.main} />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
                     <Main />
