c3a885df6789452b8b71f092f67d05a534468bd9
Sohail Saha <captainwoof.official@gmail.com>
2021-12-06 18:17:18 +0530

    Navbar bg color change invocation added

--- components/pages/about/index.tsx
+++ components/pages/about/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useNavbar } from "../../../hooks/navbar";
 import SidebarAndMain from "../../layouts/sidebarAndMain";
 import MainSeo from "../../meta/seo/main";
 import Footer from "../../molecules/footer";
@@ -5,6 +7,12 @@ import AboutBio from "./bio";
 import MyPicSidebar from "./sidebar";
 
 export default function About() {
+    // Set navbar background to primary
+    const { setNavbarTransparent } = useNavbar()
+    useEffect(() => {
+        setNavbarTransparent(false)
+    }, [setNavbarTransparent])
+
     return (
         <>
             <MainSeo />
