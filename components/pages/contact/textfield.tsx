dfa62792da0f0a174ecf75921108dcf0a9d89b2a
Sohail Saha <captainwoof.official@gmail.com>
2021-12-06 11:35:17 +0530

    Fixed type error in textfield

--- components/pages/contact/textfield.tsx
+++ components/pages/contact/textfield.tsx
@@ -3,24 +3,25 @@ import styles from './styles.module.css'
 
 interface ITextfield {
     value: string
-    onChangeTextField?: ChangeEventHandler<HTMLInputElement> | undefined
-    onChangeTextArea?: ChangeEventHandler<HTMLTextAreaElement> | undefined
+    onChangeTextField?: ChangeEventHandler<HTMLInputElement>
+    onChangeTextArea?: ChangeEventHandler<HTMLTextAreaElement>
     label: string
     name: string
     error: string | undefined | null
-    inputProps?: InputHTMLAttributes<HTMLInputElement>
+    textFieldProps?: InputHTMLAttributes<HTMLInputElement>
+    textAreaProps?: InputHTMLAttributes<HTMLTextAreaElement>
     large?: boolean
     placeholder?: string
     type?: HTMLInputTypeAttribute | undefined
 }
 
-export default function Textfield({ value, onChangeTextField, onChangeTextArea, label, name, error, inputProps, large = false, placeholder, type = 'text' }: ITextfield) {
+export default function Textfield({ value, onChangeTextField, onChangeTextArea, label, name, error, textFieldProps, textAreaProps, large = false, placeholder, type = 'text' }: ITextfield) {
     return (
         <div className={styles.textfield}>
             <label htmlFor={`${label}-input`} className={styles.label}>{label}</label>
             {large
-                ? <textarea id={`${label}-input`} className={styles.input} onChange={onChangeTextArea} value={value} name={name} {...inputProps} rows={4} />
-                : <input id={`${label}-input`} className={styles.input} onChange={onChangeTextField} value={value} name={name} placeholder={placeholder} type={type} {...inputProps} />
+                ? <textarea id={`${label}-input`} className={styles.input} onChange={onChangeTextArea} value={value} name={name} {...textAreaProps} rows={4} />
+                : <input id={`${label}-input`} className={styles.input} onChange={onChangeTextField} value={value} name={name} placeholder={placeholder} type={type} {...textFieldProps} />
             }
             {!!error &&
                 <span className={styles.error}>{error}</span>
